4d326c1a5eb7a2fa6506bfc85a396e45983179a7
dantedev24 <dante.pangione@hefti-pangione.ch>
2026-04-14 00:09:52 +0200

    typescript compilation error

--- components/ui/SubmitButton.tsx
+++ components/ui/SubmitButton.tsx
@@ -7,11 +7,13 @@ import { Loader2 } from "lucide-react";
 export default function SubmitButton({ 
   defaultText, 
   loadingText, 
-  className 
+  className,
+  disabled
 }: { 
   defaultText: string;
   loadingText: string;
   className?: string;
+  disabled?: boolean;
 }) {
   // This hook automatically knows if the parent <form> is currently running a server action!
   const { pending } = useFormStatus();
@@ -19,7 +21,7 @@ export default function SubmitButton({
   return (
     <button
       type="submit"
-      disabled={pending}
+      disabled={pending || disabled}
       className={`flex items-center justify-center gap-2 transition-colors ${className} disabled:opacity-70 disabled:cursor-not-allowed`}
     >
       {pending && <Loader2 className="h-4 w-4 animate-spin" />}
